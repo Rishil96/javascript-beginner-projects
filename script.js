957b7f350b20353d07b8a845d6f39fd21eecce23
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-07 17:27:33 +0530

    Add : Backend logic to generate a random hex color and set it as background

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get required selectors
+const hexColor = document.querySelector(".hex-color");
+const generateBtn = document.querySelector(".generate");
+
+
+// Function to generate hex color randomly
+const generateHexColor = () => {
+    const randomHexColor = Math.random().toString(16).substring(2, 8);
+    document.body.style.backgroundColor = `#${randomHexColor}`;
+    hexColor.innerHTML = `#${randomHexColor}`;
+}
+
+
+// Add event listener
+generateBtn.addEventListener("click", generateHexColor);
