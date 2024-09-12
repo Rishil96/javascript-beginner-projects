2a2fb9d2c48ba646996f30e719979e98841ff901
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-12 19:25:52 +0530

    Add : Logic to generate audio and video markup and copy the code for them to clipboard

--- script.js
+++ script.js
@@ -27,14 +27,14 @@ function generateLink(e) {
  
         downloadLink.value = getDownloadLink;
         
-        // Function to copy text to clipboard
+        // ----------- Function to copy text to clipboard ----------- 
         function copyText(target) {
             if (target.value == "") {
                 alert("Please generate a download link")
             }
             else {
                 navigator.clipboard.writeText(target.value);
-                alert("Download Link Copied!");
+                alert("Copied to clipboard!");
             }
         }
 
@@ -43,5 +43,33 @@ function generateLink(e) {
         copy.addEventListener("click", () => {
             return copyText(downloadLink);
         })
+
+        // ----------- Embed audio function ----------- 
+        const audio1 = '<audio width="300" height="32" controls="controls" src="';
+        const audio2 = '" type="audio/mp3"></audio>';
+        const embedAudio = document.getElementById("embed-audio");
+        embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
+        console.log(embedAudio.value);
+
+        // Copy embed audio code
+        const copyAudio = document.querySelector(".copy-audio");
+        copyAudio.addEventListener("click", () => {
+            return copyText(embedAudio);
+        })
+
+        // ----------- Embed video function ----------- 
+        const getVideoLink = gLink.value.replace("/view?usp=sharing", "");
+        const video1 = '<iframe src="';
+        const video2 = '/preview" width="560" height="315"></iframe>';
+        const embedVideo = document.getElementById("embed-video");
+        embedVideo.value = `${video1}${getVideoLink}${video2}`
+
+        // Copy embed video code
+        const copyVideo = document.querySelector(".copy-video");
+        copyVideo.addEventListener("click", () => {
+            return copyText(embedVideo);
+        })
+    } else {
+        alert("Please enter a valid Google drive file link.");
     }
 }
