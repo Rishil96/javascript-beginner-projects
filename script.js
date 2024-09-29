3afdd6e203d998e81fc37cbc43761328129e4947
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-29 16:22:55 +0530

    Add : Backend logic for Mobile Responsive Menu

--- script.js
+++ script.js
@@ -16,4 +16,31 @@ function smoothScroll(e) {
     document.querySelector(href).scrollIntoView({
         behavior: "smooth",
     });
+    hideMenu();
 }
+
+
+// Responsive Mobile Menu
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+
+
+const showMenu = () => {
+    hamburger.style.display = "none";
+    close.style.transform = "translateY(0)";
+    menu.style.transform = "translateY(0)";
+}
+
+
+hamburger.addEventListener("click", showMenu);
+
+const hideMenu = () => {
+    close.style.transform = "translateY(-20rem)"
+    hamburger.style.display = "block";
+    menu.style.transform = "translateY(-200%)"
+}
+
+
+close.addEventListener("click", hideMenu);
+
