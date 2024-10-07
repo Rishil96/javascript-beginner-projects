d93c02a5ddef86791393e08549fbeba02eb35c53
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-10-07 15:47:11 +0530

    Add: Javascript logic to generate random password

---
+++ script.js
@@ -0,0 +1,53 @@
+// Password Generator 
+const empty = "";
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "1234567890";
+const symbol = "!@#$%^&*=-_";
+
+const pLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
+const submit = document.getElementById("submit");
+const password = document.getElementById("password");
+const copy = document.getElementById("copy");
+
+
+submit.addEventListener("click", () => {
+    let initialPassword = empty;
+    // Add anything that is checked to build password initially
+    upperCase.checked ? (initialPassword += uCase) : "";
+    lowerCase.checked ? (initialPassword += lCase) : "";
+    pNumber.checked ? (initialPassword += number) : "";
+    pSymbol.checked ? (initialPassword += symbol) : "";
+
+    password.value = generatePassword(pLength.value, initialPassword);
+})
+
+function generatePassword(l, initialPassword) {
+    
+    let pass = "";
+
+    for (let i=0; i<l; i++) {
+        pass += initialPassword.charAt(
+            Math.floor(Math.random() * initialPassword.length)
+        )
+    }
+
+    return pass;
+}
+
+
+// Copy logic
+copy.addEventListener("click", () => {
+    if (password.value == "") {
+        alert("Please generate a password first")
+    }
+    else {
+        password.select();
+        document.execCommand("copy");
+        alert("Password has been copied to clipboard!")
+    }
+})
